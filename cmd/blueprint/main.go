3d07a0cc03bf02256e55fa1af1faca45e9c020e5
PabloGolobar <86614673+PabloGolobar@users.noreply.github.com>
2022-06-03 23:10:33 +0300

    addead swagger

--- cmd/blueprint/main.go
+++ cmd/blueprint/main.go
@@ -4,9 +4,28 @@ import (
 	"fmt"
 	"github.com/MartinHeinz/go-project-blueprint/cmd/blueprint/apis"
 	"github.com/MartinHeinz/go-project-blueprint/cmd/blueprint/config"
+	_ "github.com/MartinHeinz/go-project-blueprint/cmd/blueprint/docs"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
 )
 
+// @title Blueprint Swagger API
+// @version 1.0
+// @description Swagger API for Golang Project Blueprint.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.email martin7.heinz@gmail.com
+
+// @license.name MIT
+// @license.url https://github.com/MartinHeinz/go-project-blueprint/blob/master/LICENSE
+
+// @BasePath /api/v1
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
 	if err := config.LoadConfig("./config"); err != nil {
 		panic(fmt.Errorf("invalid application configuration: %s", err))
@@ -15,6 +34,7 @@ func main() {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	v1 := r.Group("/api/v1")
 	{
 		v1.GET("/users/:id", apis.GetUser)
--- go.mod
+++ go.mod
@@ -3,8 +3,19 @@ module github.com/MartinHeinz/go-project-blueprint
 go 1.12
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/gin-gonic/gin v1.8.0
+	github.com/go-openapi/spec v0.20.6 // indirect
+	github.com/go-openapi/swag v0.21.1 // indirect
 	github.com/jinzhu/gorm v1.9.16
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/spf13/viper v1.4.0
 	github.com/stretchr/testify v1.7.1
+	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2
+	github.com/swaggo/gin-swagger v1.4.3
+	github.com/swaggo/swag v1.8.2 // indirect
+	github.com/urfave/cli/v2 v2.8.1 // indirect
+	golang.org/x/net v0.0.0-20220531201128-c960675eff93 // indirect
+
 )
